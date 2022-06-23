c48d1d6a117fae24af59d01195bfb21e2c4a4603
vinuthagangalakunta <vinuthagangalakunta@gmail.com>
2022-06-23 12:36:42 +0530

    problem1 added

---
+++ Assignment/src/problem1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class problem1 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int arm = s.nextInt();
+		int temp = arm;
+		int sum = 0;
+		while (temp > 0) {
+			int dif = temp % 10;
+			sum = sum + dif * dif * dif;
+			temp /= 10;
+		}
+		if (arm == sum) {
+			System.out.println("Armstrong Number");
+		} else {
+			System.out.println("Not Armstrong Number");
+		}
+	}
+}
